e204e3bd99a27ee04bd2dec0397f1d0a2a51f23a
Simon Lang <simon@tinybra.in>
2016-08-05 08:03:42 +1000

    update npm deps + half working lightbox

---
+++ components/images/lightbox-gallery.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class LightboxGallery extends React.Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      index: 0,
+      isOpen: false
+    }
+  }
+}
+
+export LightboxGallery
--- pages/work/_template.jsx
+++ pages/work/_template.jsx
@@ -1,22 +1,83 @@
 import React from 'react'
 import MDReactComponent from 'markdown-react-js'
+import Lightbox from 'react-image-lightbox'
 
 import { DebugNav } from 'components/Debug'
 import { SubNav } from 'components/SubNav'
 import { Title } from 'components/Title'
-
 import mdconfig from 'components/markdown'
 
 import data from '../../lib/data.js'
 
 import './work.css'
 
-class WorkTemplate extends React.Component { // eslint-disable-line
+let images = []
+
+class WorkTemplate extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      isOpen: false,
+      index: 0,
+    }
+
+    this.openLightbox = this.openLightbox.bind(this)
+    this.closeLightbox = this.closeLightbox.bind(this)
+    this.moveNext = this.moveNext.bind(this)
+    this.movePrev = this.movePrev.bind(this)
+    this.handleImageClick = this.handleImageClick.bind(this)
+    this.handleIterate = this.handleIterate.bind(this)
+  }
+
+  openLightbox () {
+    this.setState({ isOpen: true })
+  }
+
+  closeLightbox () {
+    this.setState({ isOpen: false })
+  }
+
+  moveNext () {
+    console.log(this)
+    this.setState({ index: (this.state.index + 1) % images.length })
+  }
+
+  movePrev () {
+    this.setState({ index: (this.state.index + images.length - 1) % images.length })
+  }
+
+  handleImageClick (i, e) {
+    e.preventDefault()
+    console.log('clicky clicky', i, e)
+    console.log(this.state)
+    console.log(images)
+    this.openLightbox()
+  }
+
+  handleIterate (Tag, props, children, level) { // eslint-disable-line
+    if (Tag === 'img') {
+      const { src, alt } = props
+      const image=`/images/${props.src}.jpg`
+      const thumb=`/images/${props.src}-thumb.jpg`
+      images.push(image)
+      console.log(image, this)
+      return (
+        <a className="popup" key={src} href={image} onClick={e => this.handleImageClick(src, e)}>
+          <img src={thumb} alt={alt} />
+        </a>
+      )
+    }
+    return <Tag {...props}>{children}</Tag>
+  }
+
   render () {
     const { children, location, route, routes} = this.props // eslint-disable-line
     const nav = data.navmap[location.pathname]
 
     const rroute = routes.find(r => r.path === data.slashless(location.pathname))
+
+    images = []
 
     console.assert(nav)
 
@@ -30,7 +91,22 @@ class WorkTemplate extends React.Component { // eslint-disable-line
           markdownOptions={mdconfig.options}
           plugins={mdconfig.plugins}
           text={rroute.page.data.body}
-          onIterate={mdconfig.handleIterate}
+          onIterate={this.handleIterate}
+        />
+      )
+    }
+
+    let lightbox = []
+
+    if (this.state.isOpen) {
+      lightbox = (
+        <Lightbox
+          mainSrc={images[this.state.index]}
+          nextSrc={images[(this.state.index + 1) % images.length]}
+          prevSrc={images[(this.state.index + images.length - 1) % images.length]}
+          onCloseRequest={this.closeLightbox}
+          onMovePrevRequest={this.movePrev}
+          onMoveNextRequest={this.moveNext}
         />
       )
     }
@@ -42,6 +118,7 @@ class WorkTemplate extends React.Component { // eslint-disable-line
         <br />
         {subnav}
         {childs}
+        {lightbox}
       </div>
     )
   }
