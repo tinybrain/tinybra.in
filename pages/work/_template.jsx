6a8fea08d2a921d882d7af8ceb565e473ef555bf
Simon Lang <simon@tinybra.in>
2016-08-05 14:23:48 +1000

    lightbox and related indexing stable

--- pages/work/_template.jsx
+++ pages/work/_template.jsx
@@ -1,24 +1,19 @@
 import React from 'react'
 import MDReactComponent from 'markdown-react-js'
 import Lightbox from 'react-image-lightbox'
+import access from 'safe-access'
 import _ from 'lodash'
 
 import { DebugNav } from 'components/Debug'
 import { SubNav } from 'components/SubNav'
 import { Title } from 'components/Title'
-import mdconfig from 'components/markdown'
 
-import data from '../../lib/data.js'
+import mdconfig from 'components/markdown'
+import data from 'lib/data'
+
+import config from 'config'
 
 import './work.css'
-
-const loopy = (i, d, n) => ((i + d + n) % n)
-
-const navvy = (curr, n) => {
-  const prev = loopy(curr, -1, n)
-  const next = loopy(curr, +1, n)
-  return { curr, prev, next }
-}
 
 class WorkTemplate extends React.Component {
   constructor (props) {
@@ -30,54 +25,71 @@ class WorkTemplate extends React.Component {
     }
 
     this.images = []
-    this.imagemap = {}
 
-    this.lightboxClose = this.lightboxClose.bind(this)
-    this.lightboxNext = this.lightboxNext.bind(this)
-    this.lightboxPrev = this.lightboxPrev.bind(this)
+    // Lightbox event props
+
+    this.lightboxHandlers = {
+      onCloseRequest: this.lightboxClose.bind(this),
+      onMovePrevRequest: this.lightboxPrev.bind(this),
+      onMoveNextRequest: this.lightboxNext.bind(this),
+    }
 
     this.onImageClick = this.onImageClick.bind(this)
-    this.handleIterate = this.handleIterate.bind(this)
+    this.markdownHandler = this.markdownHandler.bind(this)
   }
+
+  // Show Lightbox
 
   onImageClick (i, e) {
     e.preventDefault()
-    console.log('clicky clicky', i)
     this.setState({ index: i, lit: true })
-
-    console.log(this.lightboxSources())
   }
 
-  indexDelta (d) {
+  // Lightbox indexing
+
+  delta (d) {
     const [i, n] = [this.state.index, this.images.length]
     return ((i + d + n) % n)
   }
 
   lightboxSources () {
-    const [prev, curr, next] = [-1, 0, 1]
-      .map(d => this.images[this.indexDelta(d)].src)
-    return { prev, curr, next }
+    if (this.images.length <= 1) {
+      const mainSrc = this.images[0].src
+      return { mainSrc }
+    }
+
+    const [prevSrc, mainSrc, nextSrc] = [-1, 0, +1]
+      .map(d => this.images[this.delta(d)].src)
+
+    return { prevSrc, mainSrc, nextSrc }
   }
+
+  // Lightbox handlers
 
   lightboxClose () {
     this.setState({ lit: false, index: 0 })
   }
 
   lightboxNext () {
-    this.setState({ index: this.indexDelta(+1) })
+    this.setState({ index: this.delta(+1) })
   }
 
   lightboxPrev () {
-    this.setState({ index: this.indexDelta(+1) })
+    this.setState({ index: this.delta(-1) })
   }
 
-  handleIterate (Tag, props, children, level) { // eslint-disable-line
+  // Lightbox images are generated specified in an array in the frontmatter
+  // which are referenced in markdown as '![lightbox](n)', and rendered here
+  // as react components.
+
+  markdownHandler (Tag, props, children, level) { // eslint-disable-line
     if (Tag === 'img') {
       let { src, alt } = props
 
       if (alt === 'lightbox') {
         let i = parseInt(src, 10)
         let img = this.images[src]
+        console.assert(img)
         return (
           <a className="popup" key={i} href={img.src} onClick={e => this.onImageClick(i, e)}>
             <img src={img.thumb} alt={img.alt} />
@@ -93,39 +105,52 @@ class WorkTemplate extends React.Component {
   }
 
   render () {
-    const { children, location, route, routes} = this.props // eslint-disable-line
+    const { children, location, route, routes } = this.props
+
+    // SubNav data is defined externally
+
     const nav = data.navmap[location.pathname]
+    console.assert(nav)
+
+    const debugnav = config.debugNav ? <DebugNav nav={nav} /> : []
+
+    // Template doesn't know about subpages so have to go looking
 
     const rroute = routes.find(r => r.path === data.slashless(location.pathname))
 
-    console.assert(nav)
+    // Build elements for non-index pages, i.e. /work/*.md
 
-    // Lightbox index from frontmatter
-
-    const lbd = rroute.page.data.lightbox
-
-    this.images = lbd ? _.map(lbd, (alt, key) => {
-      const src = `/images/${key}.jpg`
-      const thumb = `/images/${key}-thumb.jpg`
-      return { key, alt, src, thumb }
-    }) : []
-
-    // SubNav and (inefficient) markdown override
-
+    this.images = []
     let subnav = []
-    let childs = children
+    let kiddies = children
 
-    if (location.pathname !== route.path) {
-      subnav = <SubNav nav={nav} />
+    if (rroute) {
+      // Index Lightbox image paths from frontmatter
 
-      childs = (
-        <MDReactComponent
-          markdownOptions={mdconfig.options}
-          plugins={mdconfig.plugins}
-          text={rroute.page.data.body}
-          onIterate={this.handleIterate}
-        />
-      )
+      const lbdata = access(rroute, 'page.data.lightbox')
+      if (lbdata) {
+        this.images = _.map(lbdata, (alt, key) => {
+          const src = `/images/${key}.jpg`
+          const thumb = `/images/${key}-thumb.jpg`
+          return { key, alt, src, thumb }
+        })
+      }
+
+      // SubNav and (hacky) markdown override ...
+      // md wrapper should be aware of this so as to not parse twice
+
+      const mdbody = access(rroute, 'page.data.body')
+      if (location.pathname !== route.path && mdbody) {
+        subnav = <SubNav nav={nav} />
+        kiddies = (
+          <MDReactComponent
+            markdownOptions={mdconfig.options}
+            plugins={mdconfig.plugins}
+            text={mdbody}
+            onIterate={this.markdownHandler}
+          />
+        )
+      }
     }
 
     // Render Lightbox
@@ -133,29 +158,23 @@ class WorkTemplate extends React.Component {
     let lightbox = []
 
     if (this.state.lit) {
-      const lbsrc = this.lightboxSources()
-      console.log(lbsrc)
       lightbox = (
         <Lightbox
-          mainSrc={lbsrc.curr}
-          nextSrc={lbsrc.next}
-          prevSrc={lbsrc.prev}
-          onCloseRequest={this.lightboxClose}
-          onMovePrevRequest={this.lightboxPrev}
-          onMoveNextRequest={this.lightboxNext}
+          {...this.lightboxSources()}
+          {...this.lightboxHandlers}
         />
       )
     }
 
-    // All together... and 5 - 6 - se -'vn - 8
+    // All together... AND 5 - 6 - se -'vn - 8
 
     return (
       <div className="container">
-        <DebugNav nav={nav} />
+        {debugnav}
         <Title title="Work" path={route.path} />
         <br />
         {subnav}
-        {childs}
+        {kiddies}
         {lightbox}
       </div>
     )
